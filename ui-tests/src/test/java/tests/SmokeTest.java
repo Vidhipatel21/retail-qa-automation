f38c1afef7c14de1934ff60dfd7f1a027b86546b
Vidhi Patel <88839167+Vidhipatel21@users.noreply.github.com>
2025-12-27 18:01:50 -0500

    Create SmokeTest.java

---
+++ ui-tests/src/test/java/tests/SmokeTest.java
@@ -0,0 +1,10 @@
+package tests;
+
+import org.testng.annotations.Test;
+
+public class SmokeTest {
+    @Test
+    public void sampleTest() {
+        System.out.println("Smoke test running!");
+    }
+}
