dc6460986496cdbbf0a4ddbd68139cd6a510a0eb
Vidhi Patel <88839167+Vidhipatel21@users.noreply.github.com>
2025-12-27 18:07:30 -0500

    Update SmokeTest.java

--- ui-tests/src/test/java/tests/SmokeTest.java
+++ ui-tests/src/test/java/tests/SmokeTest.java
@@ -1,10 +1,22 @@
 package tests;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.testng.annotations.Test;
 
 public class SmokeTest {
+
     @Test
-    public void sampleTest() {
-        System.out.println("Smoke test running!");
+    public void openSauceDemoHomePage() {
+        WebDriverManager.edgedriver().setup();   // auto handles msedgedriver
+        WebDriver driver = new EdgeDriver();
+
+        driver.manage().window().maximize();
+        driver.get("https://www.saucedemo.com/");
+
+        System.out.println("Title is: " + driver.getTitle());
+
+        driver.quit();
     }
 }
