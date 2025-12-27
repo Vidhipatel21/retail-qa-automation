42434b488b42fe4a972eb069b311b98358902e04
Vidhi Patel <patelvidhi1949@gmail.com>
2025-12-27 18:55:06 -0500

    Setup Maven and Edge smoke test

--- ui-tests/src/test/java/tests/SmokeTest.java
+++ ui-tests/src/test/java/tests/SmokeTest.java
@@ -1,6 +1,5 @@
 package tests;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.testng.annotations.Test;
@@ -9,9 +8,10 @@ public class SmokeTest {
 
     @Test
     public void openSauceDemoHomePage() {
-        WebDriverManager.edgedriver().setup();   // auto handles msedgedriver
-        WebDriver driver = new EdgeDriver();
+        System.setProperty("webdriver.edge.driver",
+                "C:\\selenium webdriver\\edgedriver\\msedgedriver.exe");
 
+        WebDriver driver = new EdgeDriver();
         driver.manage().window().maximize();
         driver.get("https://www.saucedemo.com/");
 
